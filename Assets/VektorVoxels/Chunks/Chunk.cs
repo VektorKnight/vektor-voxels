85e71fde46ecc470a62fb59e3f007e0a9587448f
Vektor <4340480+VektorKnight@users.noreply.github.com>
2021-08-13 11:43:14 -0600

    Added some thread safety with a single writer XOR multiple reader lock.

--- Assets/VektorVoxels/Chunks/Chunk.cs
+++ Assets/VektorVoxels/Chunks/Chunk.cs
@@ -9,19 +9,13 @@ using VektorVoxels.Threading;
 using VektorVoxels.Voxels;
 using VektorVoxels.World;
 using Debug = UnityEngine.Debug;
+using Random = UnityEngine.Random;
 
 namespace VektorVoxels.Chunks {
     [RequireComponent(typeof(MeshFilter))]
     [RequireComponent(typeof(MeshRenderer))]
     [RequireComponent(typeof(MeshCollider))]
     public sealed class Chunk : MonoBehaviour {
-        private static readonly Vector2Int[] _chunkNeighbors = {
-            Vector2Int.up, 
-            Vector2Int.right, 
-            Vector2Int.down, 
-            Vector2Int.left,
-        };
-        
         [Header("Config")] 
         [SerializeField] private Material _opaqueMaterial;
         [SerializeField] private Material _alphaMaterial;
@@ -49,6 +43,17 @@ namespace VektorVoxels.Chunks {
         // Mesh data.
         private ChunkState _state;
         private bool _waitingForJob;
+        private LightPass _lightPass;
+        
+        // Thread safety.
+        private ReaderWriterLockSlim _threadLock;
+        
+        private static readonly Vector2Int[] _chunkNeighbors = {
+            Vector2Int.up, 
+            Vector2Int.right, 
+            Vector2Int.down, 
+            Vector2Int.left,
+        };
         
         // Useful accessors.
         public Vector2Int ChunkId => _chunkId;
@@ -57,7 +62,14 @@ namespace VektorVoxels.Chunks {
         public Color16[] BlockLight => _blockLight;
         public Color16[] SunLight => _sunLight;
         public ChunkState State => _state;
+        public LightPass LightPass => _lightPass;
+        public ReaderWriterLockSlim ThreadLock => _threadLock;
 
+        /// <summary>
+        /// Initializes this chunk and gets it ready for use.
+        /// Should only be called by the WorldManager instance.
+        /// </summary>
+        /// <param name="id"></param>
         public void Initialize(Vector2Int id) {
             _chunkId = id;
             
@@ -70,8 +82,8 @@ namespace VektorVoxels.Chunks {
                 _opaqueMaterial,
                 _alphaMaterial
             };
-            _meshRenderer.enabled = false;
             
+            _meshRenderer.enabled = false;
             _meshCollider.convex = false;
 
             // Mesher and lightmapper.
@@ -81,8 +93,8 @@ namespace VektorVoxels.Chunks {
                 name = $"ChunkMesh-{GetInstanceID()}",
                 indexFormat = IndexFormat.UInt32
             };
+            
             _neighborBuffer = new Chunk[4];
-
             _meshFilter.mesh = _mesh;
             _meshCollider.sharedMesh = _mesh;
             
@@ -94,8 +106,19 @@ namespace VektorVoxels.Chunks {
             _blockLight = new Color16[dataSize];
             _heightMap = new HeightData[dimensions.x * dimensions.z];
             
-            // Generate voxel data.
+            // Thread safety.
+            _threadLock = new ReaderWriterLockSlim();
+            
+            // Queue generation pass.
+            QueueGenerationPass();
+        }
+
+        private void QueueGenerationPass() {
+            _waitingForJob = true;
             GlobalThreadPool.QueueWorkItem(() => {
+                // TODO: Actual generation code or class.
+                _threadLock.EnterWriteLock();
+                var dimensions = WorldManager.Instance.ChunkSize;
                 for (var z = 0; z < dimensions.z; z++) {
                     for (var x = 0; x < dimensions.x; x++) {
                         _voxelData[VoxelUtility.VoxelIndex(x, 0, z, in dimensions)] = VoxelTable.ById(1).GetDataInstance();
@@ -103,65 +126,167 @@ namespace VektorVoxels.Chunks {
                     }
                 }
 
-                _voxelData[VoxelUtility.VoxelIndex(0, 1, 0, in dimensions)] = VoxelTable.ById(11).GetDataInstance();
-                _heightMap[VoxelUtility.HeightIndex(0, 0, dimensions.x)] = new HeightData(1, true);
-
+                if (true) {
+                    _voxelData[VoxelUtility.VoxelIndex(4, 1, 6, in dimensions)] = VoxelTable.ById((uint)(11 + (_chunkId.x % 2))).GetDataInstance();
+                    _heightMap[VoxelUtility.HeightIndex(4, 6, dimensions.x)] = new HeightData(1, true);
+                }
+                _threadLock.ExitWriteLock();
+                
                 GlobalThreadPool.QueueOnMain(OnGenerationComplete);
             });
+            _state = ChunkState.TerrainGeneration;
         }
 
-        public void OnGenerationComplete() {
-            // Queue light first pass on the threadpool.
-            _waitingForJob = true;
-            GlobalThreadPool.QueueWorkItem(() => {
-                var sw = new Stopwatch();
-                sw.Start();
-                if (_chunkId.x % 2 == 0) {
-                    _lightMapper.InitializeSunLightFirstPass(this);
-                    _lightMapper.PropagateSunLight(this);
+        private void QueueLightPass(LightPass pass) {
+            switch (pass) {
+                case LightPass.None: {
+                    break;
                 }
-                sw.Stop();
-                Debug.Log($"Light Pass 1 (Sun): {sw.ElapsedMilliseconds}ms");
-
-                sw.Reset();
-                _lightMapper.InitializeBlockLightFirstPass(this);
-                _lightMapper.PropagateBlockLight(this);
-                sw.Stop();
-                Debug.Log($"Light Pass 1 (Block): {sw.ElapsedMilliseconds}ms");
-                
-                GlobalThreadPool.QueueOnMain(OnLightFirstPassComplete);
-            });
+                case LightPass.First: {
+                    GlobalThreadPool.QueueWorkItem(() => {
+                        _threadLock.EnterWriteLock();
+                        var sw = new Stopwatch();
+                        sw.Start();
+                        _lightMapper.InitializeSunLightFirstPass(this);
+                        _lightMapper.PropagateSunLight(this);
+                        _lightMapper.InitializeBlockLightFirstPass(this);
+                        _lightMapper.PropagateBlockLight(this);
+                        sw.Stop();
+                        Debug.Log($"Light Pass 1 (Combined): {sw.ElapsedMilliseconds}ms");
+                        _threadLock.ExitWriteLock();
+                        
+                        GlobalThreadPool.QueueOnMain(() => OnLightPassComplete(LightPass.First));
+                    });
+                    break;
+                }
+                case LightPass.Second: {
+                    GlobalThreadPool.QueueWorkItem(() => {
+                        _threadLock.EnterWriteLock();
+                        var sw = new Stopwatch();
+                        sw.Start();
+                        _lightMapper.InitializeNeighborLightPass(this, new NeighborSet(_neighborBuffer), _neighborFlags);
+                        _lightMapper.PropagateSunLight(this);
+                        _lightMapper.PropagateBlockLight(this);
+                        sw.Stop();
+                        Debug.Log($"Light Pass 2 (Combined): {sw.ElapsedMilliseconds}ms");
+                        _threadLock.ExitWriteLock();
+                        
+                        GlobalThreadPool.QueueOnMain(() => OnLightPassComplete(LightPass.Second));
+                    });
+                    break;
+                }
+                case LightPass.Third: {
+                    GlobalThreadPool.QueueWorkItem(() => {
+                        _threadLock.EnterWriteLock();
+                        var sw = new Stopwatch();
+                        sw.Start();
+                        _lightMapper.InitializeNeighborLightPass(this, new NeighborSet(_neighborBuffer), _neighborFlags);
+                        _lightMapper.PropagateSunLight(this);
+                        _lightMapper.PropagateBlockLight(this);
+                        sw.Stop();
+                        Debug.Log($"Light Pass 3 (Combined): {sw.ElapsedMilliseconds}ms");
+                        _threadLock.ExitWriteLock();
+                        
+                        GlobalThreadPool.QueueOnMain(() => OnLightPassComplete(LightPass.Third));
+                    });
+                    break;
+                }
+                default: {
+                    throw new ArgumentOutOfRangeException(nameof(pass), pass, null);
+                }
+            }
             
-            _state = ChunkState.LightFirstPass;
+            _waitingForJob = true;
+            _state = ChunkState.Lighting;
         }
-
-        public void OnLightFirstPassComplete() {
-            _waitingForJob = false;
-            _state = ChunkState.WaitingForNeighbors;
-        }
-
-        public void OnLightLastPassComplete() {
+        
+        private void QueueMeshPass() {
             _waitingForJob = true;
             GlobalThreadPool.QueueWorkItem(() => {
+                _threadLock.EnterReadLock();
                 var sw = new Stopwatch();
                 sw.Start();
-                _lightMapper.InitializeNeighborLightPass(this, new NeighborSet(_neighborBuffer), _neighborFlags);
-                _lightMapper.PropagateSunLight(this);
-                _lightMapper.PropagateBlockLight(this);
-                sw.Stop();
-                Debug.Log($"Light Pass 3 (Combined): {sw.ElapsedMilliseconds}ms");
                 _mesher.GenerateMeshData(this, new NeighborSet(_neighborBuffer), _neighborFlags);
+                sw.Stop();
+                Debug.Log($"Mesh Pass: {sw.ElapsedMilliseconds}ms");
+                _threadLock.ExitReadLock();
+                
                 GlobalThreadPool.QueueOnMain(OnMeshPassComplete);
             });
 
             _state = ChunkState.Meshing;
         }
+        
+        private void OnGenerationComplete() {
+            // Queue first light first pass on the threadpool.
+            _waitingForJob = false;
+            _lightPass = LightPass.None;
+            QueueLightPass(LightPass.First);
+        }
 
-        public void OnMeshPassComplete() {
+        private void OnLightPassComplete(LightPass pass) {
+            Debug.Assert(pass > _lightPass);
+            
+            _waitingForJob = false;
+            _lightPass = pass;
+
+            _state = ChunkState.WaitingForNeighbors;
+        }
+
+        private void OnMeshPassComplete() {
             _waitingForJob = false;
             _mesher.SetMeshData(ref _mesh);
             _meshRenderer.enabled = true;
             _state = ChunkState.Ready;
+        }
+
+        private void CheckForNeighborState() {
+            _neighborFlags = NeighborFlags.None;
+            for (var i = 0; i < 4; i++) {
+                _neighborBuffer[i] = null;
+                var neighborId = _chunkId + _chunkNeighbors[i];
+
+                if (!WorldManager.Instance.ChunkInBounds(neighborId)) {
+                    continue;
+                }
+
+                if (!WorldManager.Instance.IsChunkLoaded(neighborId)) {
+                    return;
+                }
+                        
+                var neighbor = WorldManager.Instance.Chunks[neighborId.x, neighborId.y];
+
+                if (neighbor.State < ChunkState.WaitingForNeighbors || neighbor.LightPass < _lightPass) {
+                    return;
+                }
+
+                _neighborBuffer[i] = neighbor;
+                _neighborFlags |= (NeighborFlags)(1 << i);
+            }
+            
+            // Queue lighting or mesh passes based on state.
+            switch (_lightPass) {
+                case LightPass.None:
+                    break;
+                case LightPass.First:
+                    QueueLightPass(LightPass.Second);
+                    break;
+                case LightPass.Second:
+                    QueueLightPass(LightPass.Third);
+                    break;
+                case LightPass.Third:
+                    QueueMeshPass();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void OnLightSecondPassComplete() {
+            _waitingForJob = true;
+            
+
+            _state = ChunkState.Meshing;
         }
 
         private void Update() {
@@ -172,50 +297,8 @@ namespace VektorVoxels.Chunks {
                 case ChunkState.TerrainGeneration: {
                     break;
                 }
-                case ChunkState.LightFirstPass: {
-                    break;
-                }
                 case ChunkState.WaitingForNeighbors: {
-                    _neighborFlags = NeighborFlags.None;
-                    for (var i = 0; i < 4; i++) {
-                        _neighborBuffer[i] = null;
-                        var neighborId = _chunkId + _chunkNeighbors[i];
-
-                        if (!WorldManager.Instance.ChunkInBounds(neighborId)) {
-                            continue;
-                        }
-
-                        if (!WorldManager.Instance.IsChunkLoaded(neighborId)) {
-                            return;
-                        }
-                        
-                        var neighbor = WorldManager.Instance.Chunks[neighborId.x, neighborId.y];
-
-                        if (neighbor.State < ChunkState.WaitingForNeighbors) {
-                            return;
-                        }
-
-                        _neighborBuffer[i] = neighbor;
-                        _neighborFlags |= (NeighborFlags)(1 << i);
-                    }
-                    
-                    GlobalThreadPool.QueueWorkItem(() => {
-                        var sw = new Stopwatch();
-                        sw.Start();
-                        _lightMapper.InitializeNeighborLightPass(this, new NeighborSet(_neighborBuffer), _neighborFlags);
-                        _lightMapper.PropagateSunLight(this);
-                        _lightMapper.PropagateBlockLight(this);
-                        sw.Stop();
-                        Debug.Log($"Light Pass 2 (Combined): {sw.ElapsedMilliseconds}ms");
-                        
-                        GlobalThreadPool.QueueOnMain(OnLightLastPassComplete);
-                    });
-
-                    _state = ChunkState.LightSecondPass;
-                    
-                    break;
-                }
-                case ChunkState.LightSecondPass: {
+                    CheckForNeighborState();
                     break;
                 }
                 case ChunkState.Meshing: {
@@ -228,6 +311,8 @@ namespace VektorVoxels.Chunks {
                     }
                     break;
                 }
+                case ChunkState.Lighting:
+                    break;
                 default: {
                     throw new ArgumentOutOfRangeException();
                 }
---
+++ Assets/VektorVoxels/Lighting/LightPass.cs
@@ -0,0 +1,8 @@
+﻿namespace VektorVoxels.Lighting {
+    public enum LightPass {
+        None = 0,
+        First = 1,
+        Second = 2,
+        Third = 3
+    }
+}
